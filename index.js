de607bb3fd5f57d415c7f33f2129af4e991d95fc
nyakiri_margaret <margaret.karanja@student.moringaschool.com>
2022-12-01 18:03:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customer  = customerName
+function upperCaseCustomerName(){
+    return customerName = customerName.toUppercase()
+} 
+
+function setBestCustomer() {
+    bestcustomer = 'not bob'
+
+}
+function overwriteBestCustomer(newCustomer){
+    bestcustomer ='newCustomer'
+
+
+}
+ 
+const leastfavouriteCustomer = 'walked'
+favouriteCustomer = 'Mercy'
+function changeLeastFavouriteCustomer(name){
+leastfavouriteCustomer = name
+
+}
+function attemptTwoFavouriteCustomers()
